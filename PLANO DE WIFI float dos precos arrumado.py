40f38f35bacece6355869cd8f046ba204887396d
fmlipe99-x <fmlipe99@gmail.com>
2026-06-05 15:40:55 -0300

    Add files via upload

---
+++ PLANO DE WIFI float dos precos arrumado.py
@@ -0,0 +1,34 @@
+## PLANO DE WIFI
+
+print( 204 * "=")
+print("WITECH")
+print( 204 * "=")
+
+print("ola, seja bem vindo a WITECH!")
+PRECO_MB = 0.25
+nomeUsuario = input("Qual o seu nome?: ")
+mbQuantidade = int(input(f"Quantos megabytes voce pretende assinar? preco do megabyte = {(PRECO_MB)} reais : "))
+preco_total = PRECO_MB * mbQuantidade
+CONSTANTE_DO_SIM = "sim"
+
+if mbQuantidade >= 500:
+    preco_total = (PRECO_MB + 0.15) * mbQuantidade
+
+
+print(f" voce precira de {preco_total} para assinar o plano")
+
+dinheiroQuantidade = float(input("Quantos reais voce tem para assinar o plano?: "))
+
+if dinheiroQuantidade >= preco_total:
+    PagamentoEfetuacao = input("Tudo certo, efetuar o pagamento? (responda apenas com sim ou nao):" )
+    if PagamentoEfetuacao == CONSTANTE_DO_SIM:
+        print("Parabens! voce assinou o plano da WITECH!")
+    else:
+        print("okay, tenha um otimo dia!")
+else:
+    if dinheiroQuantidade < preco_total:
+        print("okay, voce e um pobre tristinho adeus!")
+
+print( 204 * "=")
+print("WITECH")
+print( 204 * "=")
